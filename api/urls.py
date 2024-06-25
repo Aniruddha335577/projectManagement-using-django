28ace550c7b57e5e2fb68afe3f2781036a305462
Aniruddha335577 <aniruddhachowdhury777@gmail.com>
2024-06-25 22:08:04 +0600

    cmt

--- api/urls.py
+++ api/urls.py
@@ -2,6 +2,9 @@ from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 from .views import UserViewSet, ProjectViewSet, ProjectMemberViewSet, TaskViewSet, CommentViewSet
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
+from rest_framework import permissions
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
 
 
 router = DefaultRouter()
@@ -11,8 +14,23 @@ router.register(r'project-members', ProjectMemberViewSet)
 router.register(r'tasks', TaskViewSet)
 router.register(r'comments', CommentViewSet)
 
+schema_view = get_schema_view(
+    openapi.Info(
+        title="Team Collaboration API",
+        default_version='v1',
+        description="API documentation for the Team Collaboration project",
+        terms_of_service="https://www.google.com/policies/terms/",
+        contact=openapi.Contact(email="contact@teamcollab.local"),
+        license=openapi.License(name="BSD License"),
+    ),
+    public=True,
+    permission_classes=(permissions.AllowAny,),
+)
+
 urlpatterns = [
     path('api/', include(router.urls)),
     path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
 ]
